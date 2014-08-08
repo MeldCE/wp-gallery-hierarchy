edabd31756d12304f3b6069696170bdb985177b1
Jack Farley <jack@meldce.com>
2014-08-08 00:38:40 -0500

    Added function and call for importing all the album scripts (and classes).

--- gallery-hierarchy.php
+++ gallery-hierarchy.php
@@ -11,6 +11,32 @@
 
 if (!class_exists('GHierarchy')) {
 	require_once('lib/GHierarchy.php');
+	require_once('lib/GHAlbum.php');
+
+	/**
+	 * Scans through a given directory and includes all php files with a given
+	 * extension
+	 */
+	function gHIncludeFiles ($path, $extension='.php') {
+		$files = scandir($path);
+		$extLength = strlen($extension);
+
+		foreach ($files as $include) {
+			if (strpos($include, '.') !== 0) { // ignores dotfiles and self/parent directories
+				if (is_dir($include)) { // if a directory, iterate into
+					$newPath = $currentPath . $include . '/';
+					includeFiles($path, $extension);
+				} else {
+					if (!$extLength || substr($include, -$extLength) === $extension) {
+						try {
+							require_once($path.$include);
+						} catch (Exception $e) { // Silently fail if the import fails
+						}
+					}
+				}
+			}
+		}
+	}
 
 	function gHierarchySetup() {
 		add_shortcode('ghalbum', array('GHierarchy', 'albumShortcode'));
@@ -26,6 +52,8 @@ if (!class_exists('GHierarchy')) {
 
 		}
 	}
+
+	gHIncludeFiles(plugin_dir_path(__FILE__) . 'albums/');
 
 	add_action('gh_rescan', array('GHierarchy', 'scan'));
 	add_action('plugins_loaded', 'gHierarchySetup');
